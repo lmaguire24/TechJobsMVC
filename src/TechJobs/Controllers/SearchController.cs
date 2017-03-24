f315b8f1c47c9c8d55bccec5414712a197db5a59
Lenon Maguire <lenon.maguire@gmail.com>
2017-03-23 18:35:15 -0700

    working on last of it

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,15 +15,28 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
-
+        [HttpPost]
         public IActionResult Results(string searchType, string searchTerm)
-       
         {
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> searchResults = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = searchResults;
+            }
+
+
+            else
+            {
+                List<Dictionary<string, string>> searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = searchResults;
+
+            }
+        }
+    } 
+                       
             ViewBag.columns = ListController.columnChoices;
             //Line below is same as Console.Writeline() (same idea, last name/first name)
-            List <Dictionary<string, string>> searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
-            ViewBag.jobs = searchResults;
+            Viewbag.title= "Search By " 
             return View("Index");
-        }
     }
 }
